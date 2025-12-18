00b73946ca21d6d0939172796a2e8e53ce2a1777
izzyromano <ysabelmarieromano@gmail.com>
2025-12-18 10:55:31 +0000

    Add files

---
+++ catrescues.js
@@ -0,0 +1,30 @@
+
+
+let ids=["manchester", "stockport", "tameside", "oldham", "rochdale", "bury", "bolton", "wigan", "salford", "trafford"];
+let dropDown = document.getElementById("chooseDist");
+
+dropDown.onchange = function(){
+const selectedOpt = this.value;
+for(let x = 0; x < ids.length; x++){
+    document.getElementById(ids[x]).style.display = "none";
+}
+if (selectedOpt === "allgm") {
+        for(let x = 0; x < ids.length; x++){
+            let showElement = document.getElementById(ids[x]);
+            if (showElement) {
+                showElement.style.display = "grid";
+            }
+        }
+    } else {
+        const showElement = document.getElementById(selectedOpt);
+        if (showElement) {
+            showElement.style.display = "grid";
+        }
+    }
+}
+
+// document.getElementById(this.value).style.display = "grid";
+
+
+// }
+
